6d72a6526241ab3989a9cf9febabefcd3692a970
Amal K Reji <amalreji45.amal@gmail.com>
2024-10-25 15:13:35 +0530

    added banner api

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import "./Banner.css";
+import axios from '../../axios';
+import {API_KEY,imageURL} from '../../constants/constants'
+
 
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0]);
+      // setMovie(response.data.results[0])
+      setMovie(response.data.results.sort((a,b)=>{return 0.5 - Math.random()})[0])
+    })
+  }, [])
+  
+  
   return (
-    <div className='banner'>
+    <div className='banner' style={{backgroundImage:`url(${movie ? imageURL+movie.backdrop_path : ""})`}}>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie ? movie.title : "Loading..."}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Ipsa quos saepe, quis dicta nihil sapiente ipsum. </h1>
+            <h1 className='description'>{movie ? movie.overview : ""}</h1>
         </div>
 
         <div className="fade_botton">
---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+
+
+export const baseURL = 'https://api.themoviedb.org/3';
+export const API_KEY =  'a7511297d427ad6aeef810b7e262bfad';
+export const imageURL = 'https://image.tmdb.org/t/p/original'
+
